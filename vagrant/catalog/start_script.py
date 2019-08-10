8fc4a67c8ddee09bc28590f82781eda0a5888884
Nikolaus Ruf <nikolaus.ruf@daimler.com>
2019-08-10 17:21:03 +0800

    changed start script to work with WSGI

--- vagrant/catalog/start_script.py
+++ vagrant/catalog/start_script.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Configure catalog app.
+
+If called directly, will also start to server the app in debug mode using
+Flask's built in server. Do not use this in production!
+
+Written by Nikolaus Ruf
+"""
 
 import logging
 from sqlalchemy import create_engine
@@ -18,17 +25,18 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
+logger.info("Configure catalog app")
+app.static_folder = "../static"
+# HTML and style files are outside the Python module directory
+app.template_folder = "../templates"
+app.secret_key = get_token()
+app.config["content"] = ContentManager(DBManager(
+    create_engine("sqlite:///data/catalog.db")
+))
+app.config["google_client_secret_file"] = "data/client_secret.json"
+
 
 if __name__ == "__main__":
-    logger.info("Configure catalog app")
+    logger.info("Start catalog app in debug mode")
     app.debug = True
-    app.static_folder = "../static"
-    # HTML and style files are outside the Python module directory
-    app.template_folder = "../templates"
-    app.secret_key = get_token()
-    app.config["content"] = ContentManager(DBManager(
-        create_engine("sqlite:///data/catalog.db")
-    ))
-    app.config["google_client_secret_file"] = "data/client_secret.json"
-    logger.info("Start catalog app")
     app.run(host='0.0.0.0', port=5000)
